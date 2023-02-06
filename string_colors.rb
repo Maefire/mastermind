09b7bfd8a871c8ef16e74caa4d88ad24b50b5b68
MaeFire <masked_phoenix@yahoo.com>
2023-02-06 01:34:23 -0600

    Add color for password and hint strings
    Added a hash that will return a colored string("1"-"6"), and a hint hash
    that will return one of two symbols, to indicate if something matches or
    not. Since I will have to likely print the full password out, I have a
    basic print method implemented. This currently just prints out the
    code_colors method. I may update it for better variety in the future.

---
+++ string_colors.rb
@@ -0,0 +1,28 @@
+# This contains a list of colors and background colors.
+# MASTERMIND arrays are specificially for use in the Mastermind game
+module StringColors
+  def code_colors(number)
+    {
+      '1' => "\e[101m 1 \e[0m", # pink
+      '2' => "\e[41m 2 \e[0m",  # red
+      '3' => "\e[42m 3 \e[0m",  # green
+      '4' => "\e[43m 4 \e[0m",  # yellow
+      '5' => "\e[44m 5 \e[0m",  # blue
+      '6' => "\e[45m 6 \e[0m"   # purple
+    }[number]
+  end
+
+  def hint_colors(hint)
+    {
+      '?' => "\e[30m \u25CF \e[0m",
+      '!' => "\e[32m \u25CF \e[0m"
+    }[hint]
+  end
+
+  def print_colored_numbers(number_array)
+    number_array.each do |number|
+      print code_colors(number), ' '
+    end
+    puts ''
+  end
+end
