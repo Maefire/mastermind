3896df3f0bf20dc1f6471e1041f5362c0a231cef
MaeFire <masked_phoenix@yahoo.com>
2023-02-07 02:07:21 -0600

    Add option for game choice
    Added the ability to choose whether player wanted to be the password
    maker, or the password breaker.

--- game.rb
+++ game.rb
@@ -8,5 +8,31 @@ class Game
 
   def play
     puts instructions
+    check_choice = maker_or_breaker?
+  end
+
+  def maker_or_breaker?
+    puts <<~HEREDOC
+      Would you prefer to be the password:
+      \e[2m(Type one of the below options)\e[0m
+      \e[1;4mMaker\e[0m
+
+      \e[1;4mBreaker\e[0m
+
+    HEREDOC
+    loop do
+      user_input = gets.chomp.downcase
+      return user_input if %w[maker breaker].include?(user_input)
+
+      puts 'Error! Please enter "Maker" or "Breaker"!'
+    end
+  end
+
+  def breaker_logic
+    # pull from logic file
+  end
+
+  def maker_logic
+    # pull from logic file
   end
 end
