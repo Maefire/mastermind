c6b62ff9d537688ceca19200ee721d734d324b71
MaeFire <masked_phoenix@yahoo.com>
2023-02-14 01:07:53 -0600

    Refactor for consistency and bug removal
    Ii realized that I was requiring extra information instead of using
    the instance variables I had already created. While this didn't fix
    or break anything, it now looks cleaner. Something about how
    Rubocop linted my code caused a game breaking bug. I fixed it, but still
    don't know what in particular caused it.

--- breaker.rb
+++ breaker.rb
@@ -14,11 +14,11 @@ class Breaker
 
   def game_loop
     @round_counter = 1
-    generate_password # delete da "p" nerd
+    generate_password
     loop do
       puts "\e[1mRound: #{@round_counter}\e[0m"
       guess_password
-      break if round_12?(@round_counter) || password_solved?(@password, @user_guess)
+      break if password_solved? || round_12?
 
       @round_counter += 1
     end
@@ -34,29 +34,29 @@ class Breaker
     puts "\e[2m(Invalid characters are ignored.)\e[0m"
     @user_guess = gets.chomp.delete('^1-6').split('')
     if @user_guess.length == 4
-      # run the comparison method here
       password_compare(@password, @user_guess)
     else
       puts 'Error! Only enter four numbers between 1-6!'
+      @round_counter -= 1
     end
   end
 
-  def password_solved?(password, guess)
-    return unless guess.eql?(password)
+  def password_solved?
+    return unless @user_guess.eql?(@password)
 
     puts "\e[102mCongratulations! You cracked the password!\e[0m"
     try_again?
   end
 
-  def round_12?(_round_counter)
-    return unless @round_counter > 12
+  def round_12?
+    return unless @round_counter >= 12
 
     puts "\e[91mHow unfortunate! The computer overlord won this time!\e[0m"
     try_again?
   end
 
   def try_again?
-    puts 'New Game? Enter "Y" to continue, or any other key to quit.'
+    puts 'New Game? Enter \"Y\" to continue, or any other key to quit.'
     new_game = gets.chomp.downcase
     if %w[y yes].include?(new_game)
       Game.new.play
