882cd5e06e0d6f189367525858430be7f072f80f
MaeFire <masked_phoenix@yahoo.com>
2023-02-16 03:12:56 -0600

    Refactor to use in maker file
    I am attempting to use a counter, so I can more easily eliminate any
    combinations that don't match. I also moved the password generation
    back into breaker file, since it was not working how I wanted.

--- game_logic.rb
+++ game_logic.rb
@@ -1,7 +1,7 @@
 require './string_colors'
 
 # this module is used to check password against guesses, then print the guess and
-# hint out for the player feedback
+    # hint out for the player feedback
 module GameLogic
   include StringColors
 
@@ -9,16 +9,18 @@ module GameLogic
     temp_password = password.clone
     temp_guess = guess.clone
     unconverted_hint = hint_checking(temp_password, temp_guess)
+    hint_count(unconverted_hint)
     print "#{colored_numbers(guess)}    hint:#{convert_hint(unconverted_hint)}\n\n"
   end
 
-  def hint_checking(password,guess)
-    exact_checking(password,guess)
-    partial_checking(password,guess)
-    @hint
+  def hint_count(array)
+    exact = array.count("!")
+    partial = array.count("?")
+    
+    total_match = [exact, partial]
   end
-
-  def exact_checking(password, guess)
+  
+  def hint_checking(password, guess)
     @hint = []
     password.each_with_index do |element, index|
       next unless guess[index] == element
@@ -27,10 +29,7 @@ module GameLogic
       password[index] = 'match'
       guess[index] = 'match'
     end
-    @hint
-  end
 
-  def partial_checking(password,guess)
     guess.each_index do |index|
       next unless guess[index] != 'match' && password.include?(guess[index])
 
@@ -45,11 +44,5 @@ module GameLogic
   def convert_hint(_hint_array)
     hint_string = @hint.map { |color| hint_colors(color) }
     hint_string.join
-  end
-
-  # this creates a random password of 4 numbers
-  def generate_password
-    @all_combos ||= @choices.repeated_permutation(4).to_a
-    @password = @all_combos.sample
   end
 end
