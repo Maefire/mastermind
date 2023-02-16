b1fac1dde7dfde36488aabb530807244de7bd20e
MaeFire <masked_phoenix@yahoo.com>
2023-02-16 03:24:02 -0600

    Finish initial gameplay setup for breaker play
    maker file not complete. Breaker available to play.

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
-require_relative 'string_colors.rb'
-require_relative 'instruction_page.rb'
-require_relative 'game.rb'
+require_relative 'string_colors'
+require_relative 'instruction_page'
+require_relative 'game'
+require_relative 'breaker'
+require_relative 'game_logic'
+require_relative 'maker.rb'
 
 Game.new.play
